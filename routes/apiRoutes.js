e97c48d925da205b5fdd93a6811e7acacd8e206f
Josh Herrera <joshherrera09@gmail.com>
2020-10-28 16:42:32 -0700

    apiRoutes in process

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,49 @@
 const route = require("express").Router();
 
-const workout = require ("../models/workout.js");
+const Workout = require("../models/workout.js");
+
+route.post('/api/workouts', function (request, response) {
+    // get all body stuff from request.body object
+    // get all request parameter stuff from request.params object
+    Workout.create({})
+        .then(dbWorkout => {
+            response.json(dbWorkout)
+        }).catch(err => {
+            response.json(err)
+        });
+});
+
+app.put('/api/workouts/:id', function ({ body, params}, response) {
+    // get all body stuff from request.body object
+    // get all request parameter stuff from request.params object
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: {exercise: body}},
+        { new: true, runValidators: true}
+    ).then(dbWorkout => {
+        response.json(dbWorkout);
+    }).catch(err => {
+        response.json(err)
+    });
+});
+
+route.get('/api/workouts', function(request, response) {
+    Workout.find()
+        .then(dbWorkouts => {
+            response.json(dbWorkouts)
+        }).catch(err => {
+            escape.json(err)
+        });
+});
+
+app.get('/api/workouts/range', function (request, response) {
+    // get all body stuff from request.body object
+    // get all request parameter stuff from request.params object
+    Workout.find({}).limit(7)
+        .then(dbWorkouts => {
+            console.log(dbWorkouts);
+            response.json(dbWorkouts)
+        }).catch(err => {
+            response.json(err)
+        });
+});
--- server.js
+++ server.js
@@ -17,11 +17,6 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
 
-
-
-
-
-
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
   });
